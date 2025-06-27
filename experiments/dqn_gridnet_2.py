fc78b0c24cb0031bdbb890c46cd6d0f76e067a17
rmehempel1 <hempel.robert@gmx.net>
2025-06-27 18:09:21 +0200

    dqn_gridnet_2.py debug

--- experiments/dqn_gridnet_2.py
+++ experiments/dqn_gridnet_2.py
@@ -871,7 +871,8 @@ if __name__ == "__main__":
 
             mean_reward = np.mean(total_rewards[-100:])
             print(
-                f"Episode {episode_idx}, Frame {frame_idx}: Reward={reward:.2f}, Mean(100)={mean_reward:.2f}, Epsilon={epsilon:.2f}")
+                f"Episode {episode_idx}, Frame {frame_idx}: Reward={reward.item():.2f}, Mean(100)={mean_reward.item():.2f}, Epsilon={epsilon.item():.2f}"
+            )
 
             if best_mean_reward is None or best_mean_reward < mean_reward:
                 print(f"Neues bestes Ergebnis: {best_mean_reward} → {mean_reward:.2f}")
