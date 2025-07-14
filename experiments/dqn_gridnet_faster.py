bf85ac14b13868513e11aa85b425ae0a5ce6286e
rmehempel1 <hempel.robert@gmx.net>
2025-07-14 16:31:20 +0200

    dqn_gridnet_faster.py logging debug

--- experiments/dqn_gridnet_faster.py
+++ experiments/dqn_gridnet_faster.py
@@ -1195,7 +1195,7 @@ if __name__ == "__main__":
         partial_obs=args.partial_obs,
         max_steps=2000,
         render_theme=2,
-        ai2s=[microrts_ai.workerRushAI],
+        ai2s=[microrts_ai.passiveAI for _ in range(args.num_bot_envs)],
         map_paths=[args.train_maps[0]],
         reward_weight=np.array([300.0, 10.0, 40.0, -100.0, 50.0, -100.0]),
         cycle_maps=args.train_maps
