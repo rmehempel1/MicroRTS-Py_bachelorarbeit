4101c86900feafdeceed2ae93c6ab5301777ec5b
rmehempel1 <hempel.robert@gmx.net>
2025-08-10 21:43:13 +0200

    deep_recurrent_q_net.py hinzugefügt

--- experiments/deep_recurrent_q_net.py
+++ experiments/deep_recurrent_q_net.py
@@ -1120,7 +1120,7 @@ if __name__ == "__main__":
     # Training
     while frame_idx < args.total_timesteps:
         frame_idx += 1
-        if frame_idx % 100 == 0:
+        if frame_idx % 1000 == 0:
             print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Frame idx: {frame_idx}")
         epsilon = max(args.epsilon_final, args.epsilon_start - frame_idx / args.epsilon_decay)
         if frame_idx < warmup_frames:
